24b5ecda9ee54e03882e5ef6ea8882251f81efac
TeslenkoRoman <drrvt@rambler.ru>
2020-04-16 23:32:39 +0300

    File for lesson 4

--- main.py
+++ main.py
@@ -54,3 +54,5 @@ only_date.sort(key=lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S'), reverse=
 
 print("Дата и время самого позднего лога: ", only_date[0])
 print("Дата самого позднего лога: ", only_date[0].split(' ')[0])
+
+# все
